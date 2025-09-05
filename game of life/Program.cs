b20fb4b82c11716f83da919a0019d24b23bf7bf7
Vincent Bouvier <wyzgo45@gmail.com>
2025-09-06 01:41:29 +0200

    adding 4th state

--- game of life/Program.cs
+++ game of life/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     // --- Environment variables ---
     public const int l = 300;
     public const int L = 150;
-    public static int AverageCells = l * L / 5;
+    public static int AverageCells = 5000;
     public static int SleepTime = 80;
     public static int[,] map = new int[L, l];
 
@@ -52,10 +52,10 @@ internal class Program
         {
             Random rand = new Random();
 
-            int x = rand.Next(10, L - 10);
-            int y = rand.Next(10, l - 10);
+            int x = rand.Next(30, L - 30);
+            int y = rand.Next(30, l - 30);
 
-            map[x, y] = 2;
+            map[x, y] = 3;
         }
 
     }
@@ -75,6 +75,7 @@ internal class Program
     }
 
     // --- Colors ---
+    const string RED = "\u001b[31m";
     const string GREEN = "\u001b[32m";
     const string YELLOW = "\u001b[33m";
     const string WHITE = "\u001b[37m";
@@ -88,10 +89,12 @@ internal class Program
         {
             for (int j = 0; j < l; j++)
             {
-                if (map[i, j] == 2) 
+                if (map[i, j] == 3)
                     sb.Append(GREEN + "██" + RESET);
-                else if (map[i, j] == 1) 
+                else if (map[i, j] == 2) 
                     sb.Append(YELLOW + "██" + RESET);
+                else if (map[i, j] == 1) 
+                    sb.Append(RED + "██" + RESET);
                 else if (map[i, j] == -1) 
                     sb.Append(WHITE + "██" + RESET);
                 else 
@@ -108,51 +111,81 @@ internal class Program
         int[,] lastmap = new int[L, l];
         Array.Copy(map, lastmap, map.Length);
 
-        for (int i = 1; i < L - 1; i++)
+        for (int i = 2; i < L - 2; i++)
         {
-            for (int j = 1; j < l - 1; j++)
+            for (int j = 2; j < l - 2; j++)
             {
-                int alivecells = 0;
-                int semiAliveCells = 0;
+                // distance = 1
+                int thrivingNear = 0;
+                int matureNear = 0;
+                int weakNear = 0;
 
-                // count neighbors
-                for (int x = -1; x <= 1; x++)
+                // distance = 2
+                int thrivingFar = 0;
+                int matureFar = 0;
+                int weakFar = 0;
+
+                for (int dx = -2; dx <= 2; dx++)
                 {
-                    for (int y = -1; y <= 1; y++)
+                    for (int dy = -2; dy <= 2; dy++)
                     {
-                        if (x == 0 && y == 0) continue;// skip itself
-                        int val = lastmap[i + x, j + y];
-                        if (val == 2) alivecells++;
-                        else if (val == 1) semiAliveCells++;
+                        if (dx == 0 && dy == 0) continue; // skip self
+                        if (Math.Abs(dx) == 2 && Math.Abs(dy) == 2) continue; // skip corners
+
+                        int state = lastmap[i + dx, j + dy];
+                        int dist;
+                        if (dx == -2 || dx == 2 || dy == 2 || dy == -2)
+                            dist = 2;
+                        else
+                            dist = 1;
+
+                        if (dist == 1)
+                        {
+                            if (state == 3) thrivingNear++;
+                            else if (state == 2) matureNear++;
+                            else if (state == 1) weakNear++;
+                        }
+                        else if (dist == 2)
+                        {
+                            if (state == 3) thrivingFar++;
+                            else if (state == 2) matureFar++;
+                            else if (state == 1) weakFar++;
+                        }
                     }
                 }
 
-                if (lastmap[i, j] == 2) // alive
-                {
-                    if (alivecells < 2 || alivecells > 3)
-                        map[i, j] = 1;
-                    else
-                        map[i, j] = 2;
+                double score = (thrivingNear + 1.5 * thrivingFar) + (matureNear + 1 * matureFar) +(weakNear + 0.5 * weakFar);
+
+                if (map[i, j] == 3) // Thriving
+                { 
+                    map[i, j] = (score < 10 || score > 22) ? 2 : 3;
                 }
-                else if (lastmap[i, j] == 1) // semi
-                {
-                    if (alivecells == 3)
-                        map[i, j] = 2;
-                    else if (semiAliveCells >= 4 && alivecells < 2)
-                        map[i, j] = 1;
-                    else
-                        map[i, j] = 0;
+                else if (map[i, j] == 2) // Mature
+                { 
+                    if (score < 8 || score > 24) map[i, j] = 1;
+                    else if (score>=23) map[i, j] = 3;
+                    else if (score >= 18 && score <= 22) map[i, j] = 3;
+                    else if (thrivingNear >= 7 || matureNear >= 6 || thrivingFar >= 6 || matureFar >= 5) map[i, j] = 3;
+                    else map[i, j] = 2;
                 }
-                else if (lastmap[i, j] == 0) // dead
+                else if (map[i, j] == 1) // Weak
                 {
-                    if (alivecells == 3)
-                        map[i, j] = 2;
-                    else if (alivecells == 2 && semiAliveCells >= 1)
-                        map[i, j] = 1;
+                    if (score < 6) map[i, j] = 0;
+                    else if (score>=17) map[i, j] = 2;
+                    else if (score >= 15 && score <= 21) map[i, j] = 2;
+                    else if (thrivingNear >= 6 || matureNear >= 5 || thrivingFar>=6 || matureFar>=5) map[i, j] = 2;
+                    else map[i, j] = 1;
                 }
+                else if (map[i, j] == 0) // Dead
+                { 
+                    map[i, j] = (score >= 8 && score <= 18) ? 1 : 0;
+                }
+
+
             }
         }
     }
+
 
 
     /* Basic game
